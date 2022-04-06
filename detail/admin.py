afe0584b869b4db34ffdf6343a00d2bd0d36433f
deerr1 <furagkin678@gmail.com>
2022-04-06 13:18:10 +0500

    more changed

--- detail/admin.py
+++ detail/admin.py
@@ -1,7 +1,10 @@
-from django.db.models import Count, Sum, Min, Max, DateTimeField, F, ExpressionWrapper
+from django.db.models import Sum,  F
 from django.db.models.functions import Trunc
 from django.contrib import admin
 from django.contrib.auth.models import User, Group
+from django.contrib.admin.models import LogEntry, DELETION
+from django.contrib.contenttypes.models import ContentType
+from django.utils.encoding import force_text
 
 from detail import models
 
@@ -26,8 +29,12 @@ class SuplierModelAdmin(admin.ModelAdmin):
         'phone_number'
     ]
 
+    def has_delete_permission(self, request, obj=None):
+        return False
+
 @admin.register(models.Details)
 class DetailsModelAdmin(admin.ModelAdmin):
+    exclude  = ['is_active']
     list_display = [
         'name',
         'vendore_code'
@@ -37,8 +44,44 @@ class DetailsModelAdmin(admin.ModelAdmin):
         'vendore_code'
     ]
 
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).filter(is_active = True)
+
+@admin.register(models.DeletedDetails)
+class DeleteDetailsModelAdmin(admin.ModelAdmin):
+    exclude  = ['is_active']
+    readonly_fields = ['name', 'vendore_code']
+
+    def has_view_permission(self, request, obj=None):
+        return False
+
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+        return actions
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).filter(is_active = False)
+
 @admin.register(models.SupplierDetails)
 class SupplierDetailsModelAdmin(admin.ModelAdmin):
+    exclude  = ['is_active', 'deleted_datetime']
     list_display = [
         'supplier',
         'detail',
@@ -51,38 +94,91 @@ class SupplierDetailsModelAdmin(admin.ModelAdmin):
         'supplier__name',
         'detail__name',
         'detail__vendore_code',
-        # 'cost'
     ]
     raw_id_fields = [
         'supplier',
         'detail'
     ]
 
+    def delete_queryset(self, request, queryset):
+        for obj in queryset:
+            self.delete_model(request, obj)
+
+    def delete_model(self, request, obj):
+        LogEntry.objects.log_action(
+            user_id=request.user.pk,
+            content_type_id= ContentType.objects.get_for_model(obj).pk,
+            object_id=obj.pk,
+            object_repr=force_text(obj),
+            action_flag=DELETION
+        )
+        obj.delete()
+
     def get_readonly_fields(self, request, obj=None):
         if obj: # editing an existing object
-            return self.readonly_fields + tuple(['cost'])
+            return self.readonly_fields + tuple(['supplier','detail','cost'])
         return self.readonly_fields
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).filter(detail__is_active = True, is_active = True)
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+@admin.register(models.DeletedSupplierDetails)
+class DeletedSupplierDetailsModelAdmin(admin.ModelAdmin):
+    exclude  = ['is_active']
+    readonly_fields = [
+        'supplier',
+        'detail',
+        'cost',
+        'deleted_datetime'
+    ]
+
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+        return actions
+
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).filter(is_active = False)
 
 @admin.register(models.Purchases)
 class PurchasesModelAdmin(admin.ModelAdmin):
+    # change_list_template = 'admin/supplier_detatil_model_list.html'
+    ordering = ('-date',)
     date_hierarchy = 'date'
     list_display = [
         'suplier_detail',
         'quantity',
+        'sum',
         'date'
     ]
     list_filter = [
         'suplier_detail__supplier__name',
-        'date'
-    ]
-    search_fields = [
-        'suplier_detail__detail__name',
-        'quantity'
     ]
     raw_id_fields = [
         'suplier_detail' ,
     ]
 
+    def get_readonly_fields(self, request, obj=None):
+        if obj: # editing an existing object
+            return self.readonly_fields + tuple(['suplier_detail','quantity','date'])
+        return self.readonly_fields
+
+    def has_change_permission(self, request, obj=None):
+        return False
 
 @admin.register(models.PurchasesSummary)
 class PurchasesSummaryAdmin(admin.ModelAdmin):
@@ -92,6 +188,11 @@ class PurchasesSummaryAdmin(admin.ModelAdmin):
         'suplier_detail__supplier__name',
     ]
 
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
 
     def changelist_view(self, request, extra_context=None):
         response = super().changelist_view(
--- detail/models.py
+++ detail/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from django.core.validators import RegexValidator, MinValueValidator
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+from datetime import datetime
+
 
 class Supliers(models.Model):
     name = models.CharField(verbose_name='Название', max_length=50)
     address = models.TextField(verbose_name='Адрес')
     phone_regex = RegexValidator(regex=r'^\+\d{8,15}$', message="Номер телефона необходимо вводить в формате: «+999999999». Допускается до 15 цифр.")
-    phone_number = models.CharField(verbose_name='Номер телефона', validators=[phone_regex], max_length=16)
-
+    phone_number = models.CharField(verbose_name='Номер телефона', validators=[phone_regex], max_length=16, help_text='Например +795271941111')
     class Meta:
         verbose_name = 'Поставщик'
         verbose_name_plural = 'Поставщики'
@@ -16,7 +19,8 @@ class Supliers(models.Model):
 
 class Details(models.Model):
     name = models.CharField(verbose_name='Название детали', max_length=50)
-    vendore_code = models.CharField(verbose_name='Артикул', max_length=50)
+    vendore_code = models.CharField(verbose_name='Артикул', max_length=50, unique=True)
+    is_active = models.BooleanField(verbose_name='Активный товар или нет', default=True)
 
     class Meta:
         verbose_name = 'Деталь'
@@ -25,23 +29,53 @@ class Details(models.Model):
     def __str__(self) -> str:
         return f'{self.name} {self.vendore_code}'
 
+    def delete(self, instanse, *args, **kwargs):
+        self.is_active = False
+        super(Details, self).save(*args, **kwargs)
+
+class DeletedDetails(Details):
+
+    class Meta:
+        proxy = True
+        verbose_name = 'Удаленная деталь'
+        verbose_name_plural = 'Удаленные детали'
+
 class SupplierDetails(models.Model):
     supplier = models.ForeignKey(Supliers, on_delete=models.CASCADE, verbose_name='Поставщик')
     detail = models.ForeignKey(Details, on_delete=models.CASCADE, verbose_name='Деталь')
     cost = models.FloatField(validators=[MinValueValidator(0)], verbose_name='Цена')
+    is_active = models.BooleanField(verbose_name='Активный товар или нет', default=True)
+    deleted_datetime = models.DateTimeField(verbose_name='Дата и время удаления', blank=True, null=True, default=None)
 
     class Meta:
         verbose_name = 'Детали поставщиков'
         verbose_name_plural = 'Детали поставщиков'
 
     def __str__(self) -> str:
-        return f'{self.supplier}, {self.detail}, {self.cost}'
+        return f'{self.supplier}, Деталь: {self.detail}, Цена: {self.cost}'
+
+    def delete(self, *args, **kwargs):
+        self.is_active = False
+        self.deleted_datetime = datetime.utcnow()
+        super(SupplierDetails, self).save(*args, **kwargs)
+
+    @receiver(post_delete, sender=Details)
+    def deleted_detail(self, *args, **kwargs):
+        self.is_active = False
+        self.deleted_datetime = datetime.now()
+        super(SupplierDetails, self).save(*args, **kwargs)
+
+class DeletedSupplierDetails(SupplierDetails):
+
+    class Meta:
+        proxy = True
+        verbose_name = 'Удаленные детали поставщиков'
+        verbose_name_plural = 'Удаленные детали поставщиков'
 
 class Purchases(models.Model):
     suplier_detail = models.ForeignKey(SupplierDetails, on_delete=models.CASCADE, verbose_name='Деталь')
     quantity = models.IntegerField(validators=[MinValueValidator(0)], verbose_name='Количество')
     date = models.DateField(verbose_name='Дата покупки')
-
     class Meta:
         verbose_name = 'Покупки'
         verbose_name_plural = 'Покупки'
@@ -50,6 +84,9 @@ class Purchases(models.Model):
     def __str__(self) -> str:
         return f'{self.suplier_detail}, кол-во: {self.quantity}, дата: {self.date}'
 
+    def calculate_sum(self):
+        return self.suplier_detail.cost * self.quantity
+    sum = property(calculate_sum)
 
 class PurchasesSummary(Purchases):
 
